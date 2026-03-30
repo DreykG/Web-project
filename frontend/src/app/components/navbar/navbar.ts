2340966b01f12c8a6d20ddb62152c888e6f12804
TIMUR <kimtimur1999@gmail.com>
2026-03-30 19:32:45 +0500

    base settings for app body & navbar

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-
+  logout() {
+    localStorage.removeItem('token');
+    window.location.href = '/login';
+  }
 }
