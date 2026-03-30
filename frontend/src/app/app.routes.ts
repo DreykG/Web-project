e2ab8f30409e2a7ac5f78069030e69c8f5d34514
TIMUR <kimtimur1999@gmail.com>
2026-03-30 10:47:22 +0500

    create routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {Login} from "./pages/login/login";
+import {Shop} from "./pages/shop/shop";
+import {Cart} from "./pages/cart/cart";
+import {Inventory} from "./pages/inventory/inventory"; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'shop', pathMatch: 'full'},
+    {path: 'login', component: Login},
+    {path: 'shop', component: Shop},
+    {path: 'cart', component: Cart},
+    {path: 'inventory', component: Inventory},
+];
