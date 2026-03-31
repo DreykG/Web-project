4768cff336e8eebff1ce57c810d21c16ec690e19
TIMUR <kimtimur1999@gmail.com>
2026-03-31 11:44:17 +0500

    setting cart segment

--- frontend/src/app/pages/cart/cart.ts
+++ frontend/src/app/pages/cart/cart.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ShopService } from '../../services/shop';
 @Component({
   selector: 'app-cart',
   imports: [],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
-export class Cart {
+export class Cart implements OnInit{
+  cartItems: any[] = [];
+
+  constructor(private shopService: ShopService) {}
+
+  ngOnInit() {
+    this.shopService.getCartItems().subscribe({
+      next: (data:any) => {
+        this.cartItems = data;
+      },
+      error: () => {
+        console.log("Cart Items loading error");
+      }
+    });
+  }
+
+  removeFromCart(itemId: number) {
+    this.shopService.removeFromCart(itemId).subscribe({
+      next: (data:any) => {
+        this.cartItems = this.cartItems.filter(item => item.id !== itemId);
+      },
+      error: () => {
+        console.log("Item remove error");
+        
+      }
+    });
+  }
+
+  buyCart() {
+    this.shopService.buyCart().subscribe({
+      next: () => {
+        console.log("Items have been successfully purchased");
+      },
+      error: () => {
+        console.log("Purchase error");
+      }
+    });
+  }
 
 }
