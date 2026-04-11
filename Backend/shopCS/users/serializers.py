44225e17f61107103976ffb18b356259e225b854
TIMUR <kimtimur1999@gmail.com>
2026-04-12 01:08:36 +0500

    remove db.sqlite3 from git

--- Backend/shopCS/users/serializers.py
+++ Backend/shopCS/users/serializers.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
 from .models import Profile
 from django.db.models import Sum
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
 
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
--- Backend/shopCS/users/views.py
+++ Backend/shopCS/users/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .serializers import ProfileSerializer, RegisterSerializer
 
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
@@ -15,3 +18,11 @@ class ProfileViewSet(viewsets.ViewSet):
         
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+    
+@api_view(["POST"])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response({"detail": "User registered successfully"}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
