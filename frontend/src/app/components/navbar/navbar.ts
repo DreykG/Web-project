a5946316db9a9d1e6e9904d7a79b18b158421063
TIMUR <kimtimur1999@gmail.com>
2026-04-13 20:11:20 +0500

    upgrade navbar

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
 import { RouterLink} from '@angular/router';
+import { ProfileService } from '../../services/profile';
+import { UserProfile } from '../../interfaces/models';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +9,25 @@ import { RouterLink} from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
+export class Navbar implements OnInit {
+  profile: UserProfile | null = null;
+  errorMessage = '';
+
+  constructor(private profileService: ProfileService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.profileService.getProfile().subscribe({
+      next: (data: UserProfile) => {
+        this.profile = data;
+        this.cdr.detectChanges();
+      },
+      error: () => {
+        this.errorMessage = 'Profile loading error';
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
   logout() {
     localStorage.removeItem('token');
     window.location.href = '/login';
