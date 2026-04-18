26372bba3ffaa250c35c9be38c1f65f2691f19da
TIMUR <kimtimur1999@gmail.com>
2026-04-18 13:57:20 +0500

    update navbar-links

--- frontend/src/app/pages/case/case.ts
+++ frontend/src/app/pages/case/case.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { CaseService } from '../../services/case';
+import { Case, LiveDrop } from '../../interfaces/models';
 
 @Component({
   selector: 'app-case',
@@ -6,6 +9,52 @@ import { Component } from '@angular/core';
   templateUrl: './case.html',
   styleUrl: './case.css',
 })
-export class Cases {
+export class Cases implements OnInit {
+  cases: Case[] = [];
+  liveDrops: LiveDrop[] = [];
+  isLoading = true;
+  error: string | null = null;
 
+  constructor(
+    private caseService: CaseService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.loadCases();
+    this.loadLiveDrops();
+  }
+
+  loadCases() {
+    this.caseService.getCases().subscribe({
+      next: (data) => {
+        this.cases = data.filter(c => c.is_active);
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      },
+      error: () => {
+        this.error = 'Failed to load cases';
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  loadLiveDrops() {
+    this.caseService.getLiveDrops().subscribe({
+      next: (data) => {
+        this.liveDrops = data;
+        this.cdr.detectChanges();
+      },
+      error: () => {
+        this.error = 'Failed to load live drops';
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  goToCase(c: Case) {
+    this.router.navigate(['/cases', c.id]);
+  }
 }
